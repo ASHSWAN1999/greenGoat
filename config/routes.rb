41eb5bc3f53709a803e611fef7e07bf1beae8ef3
Ashley Swanson <ashswan.design@gmail.com>
2021-08-01 10:17:45 -0600

    styling for products page almost finished

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
   get 'donate', to: 'pages#donate'
-  resources :products, only: [:show]
+  resources :products, only: [:show, :index]
 end
