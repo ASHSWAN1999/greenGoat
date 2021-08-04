e97a141e69b9048a713fffdbb8787dcb73dbe85a
Ashley Swanson <ashswan.design@gmail.com>
2021-08-04 11:44:58 -0600

    added product details page styling

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,7 +8,10 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
+import "custom/lightbox.js"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
+
+
