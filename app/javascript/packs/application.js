e65ddb5192d7dbd891ae37cf419058752f1c23ac
Ashley Swanson <ashswan.design@gmail.com>
2021-08-18 14:41:00 -0600

    fixed it

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,8 +7,9 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
+import "packs/custom.js"
 import "bootstrap"
-import "custom/lightbox.js"
+
 
 Rails.start()
 Turbolinks.start()
--- app/javascript/packs/custom.js
+++ app/javascript/packs/custom.js
@@ -4,3 +4,13 @@ $(document).ready(function () {
       $("#mdb-lightbox-ui").load("mdb-addons/mdb-lightbox-ui.html");
     });
   });
+
+  window.onload = function() {
+    if (window.jQuery) {  
+        // jQuery is loaded  
+        console.log("jQuery has loaded!");
+    } else {
+        // jQuery is not loaded
+        console.log("jQuery has not loaded!");
+    }
+}
