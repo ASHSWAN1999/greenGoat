cbbc77a360db449d5f5a5d267156d49094fa8671
Ashley Swanson <ashswan.design@gmail.com>
2021-08-18 10:50:39 -0600

    bootstrap removed

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,11 +7,7 @@ import Rails from "@rails/ujs"
 import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
-import "bootstrap"
-import "custom/lightbox.js"
 
 Rails.start()
 Turbolinks.start()
 ActiveStorage.start()
-
-
